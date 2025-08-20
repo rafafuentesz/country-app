cbd1ea08c98770cf022a94f2785d5c766bfe54f4
rafafuentesz <rafafuentes110@gmail.com>
2025-08-20 12:00:40 -0300

    feat: implement CountryLayout component and update routing structure

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,21 +1,26 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { CountryLayoutComponent } from './layouts/CountryLayout/CountryLayout.component';
+import { Component } from '@angular/core';
 
 export const countryRoutes: Routes = [
-
   {
     path: '',
-    component: ByCapitalPageComponent,
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      },
+    ],
   },
   // {
   //   path: 'country'
   // },
-  {
-    path: '**',
-    redirectTo: ''
-  }
-
-
 ];
 
-export default countryRoutes
+export default countryRoutes;
