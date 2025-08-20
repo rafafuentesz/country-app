5610b943a2741da45c7c5fe9b48dfb8867824c96
rafafuentesz <rafafuentes110@gmail.com>
2025-08-20 11:48:04 -0300

    feat: add routing and components for country and home pages

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+
+export const countryRoutes: Routes = [
+
+  {
+    path: '',
+    component: ByCapitalPageComponent,
+  },
+  // {
+  //   path: 'country'
+  // },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+
+
+];
+
+export default countryRoutes
