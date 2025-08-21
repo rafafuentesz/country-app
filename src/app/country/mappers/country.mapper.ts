24ecde4d9d98c10a8d3cc0647918da0abaa32dd2
rafafuentesz <rafafuentes110@gmail.com>
2025-08-21 11:03:50 -0300

    feat: implement country service, update country list and add REST interfaces

---
+++ src/app/country/mappers/country.mapper.ts
@@ -0,0 +1,21 @@
+import type { Country } from '../interfaces/country.interface';
+import type { RESTCountry } from '../interfaces/rest-countries.interface';
+
+export class CountryMapper {
+  static mapRestCountryToCountry(restCountry: RESTCountry): Country {
+    return {
+      capital: restCountry.capital[0],
+      cca2: restCountry.cca2,
+      flag: restCountry.flag,
+      flagSvg: restCountry.flags.svg,
+      name: restCountry.name.common,
+      population: restCountry.population,
+    };
+  }
+
+  static mapRestCountryArrayToCountryArray(
+    restCountries: RESTCountry[]
+  ): Country[] {
+    return restCountries.map(this.mapRestCountryToCountry);
+  }
+}
--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { SearchInputComponent } from "../../components/search-input/search-input.component";
-import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { Component, inject, signal } from '@angular/core';
+import { SearchInputComponent } from '../../components/search-input/search-input.component';
+import { CountryListComponent } from '../../components/country-list/country-list.component';
+import { CountryService } from '../../services/country.service';
+import { RESTCountry } from '../../interfaces/rest-countries.interface';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -8,7 +10,21 @@ import { CountryListComponent } from "../../components/country-list/country-list
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
-  onSearchByCapital(value: string) {
-    console.log({value});
+  CountryService = inject(CountryService);
+
+  isLoading = signal(false);
+  isError = signal<string | null>(null);
+  countries = signal<RESTCountry[]>([]);
+
+  onSearchByCapital(query: string) {
+    if (this.isLoading()) return;
+    this.isLoading.set(true);
+    this.isError.set(null);
+
+    this.CountryService.searchByCapital(query).subscribe((countries) => {
+      this.isLoading.set(false);
+      this.countries.set(countries);
+      console.log({ countries });
+    });
   }
- }
+}
