7832cc11b86a205b34b7b1e1cfc98de86705de9b
rafafuentesz <rafafuentes110@gmail.com>
2025-08-22 11:36:01 -0300

    feat: mejorar manejo de errores y mensajes en la lista de países, agregar lógica de carga y vacío

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-country-page',
+  standalone: true,
   imports: [],
   templateUrl: './country-page.component.html',
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+  private route = inject(ActivatedRoute);
+  private countryService = inject(CountryService);
 
-  
+  isLoading = signal(false);
+  isError = signal<string | null>(null);
+  country = signal<Country | null | undefined>(null);
 
- }
+  ngOnInit() {
+    const code = this.route.snapshot.params['code'];
+    if (code) {
+      this.onSearchCountryByAlphaCode(code);
+    }
+  }
+
+  onSearchCountryByAlphaCode(code: string) {
+    if (this.isLoading()) return;
+    this.isLoading.set(true);
+    this.isError.set(null);
+
+    this.countryService.searchCountryByAlphaCode(code).subscribe({
+      next: (country) => {
+        this.isLoading.set(false);
+        this.country.set(country);
+        console.log({ country });
+      },
+      error: (err: Error) => {
+        this.isLoading.set(false);
+        this.country.set(null);
+        this.isError.set(err.message);
+      },
+    });
+  }
+}
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { map, Observable, catchError, throwError } from 'rxjs';
+import { map, Observable, catchError, throwError, delay } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -15,30 +15,43 @@ export class CountryService {
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase();
 
-    return this.http
-      .get<RESTCountry[]>(`${API_URL}/capital/${query}`)
-      .pipe(
-        map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
-        catchError((error) => {
-          console.error('Error fetching', error);
-          return throwError(() => new Error(`No se encontró un país con esa capital: ${query}`));
-        })
-      );
+    return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
+      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      catchError((error) => {
+        console.error('Error fetching', error);
+        return throwError(
+          () => new Error(`No se encontró un país con esa capital: ${query}`)
+        );
+      })
+    );
   }
-
 
   //buscar por país con: const url = `${API_URL}/name/${query}`
   searchByCountry(query: string): Observable<Country[]> {
     query = query.toLowerCase();
 
-    return this.http
-      .get<RESTCountry[]>(`${API_URL}/name/${query}`)
-      .pipe(
-        map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
-        catchError((error) => {
-          console.error('Error fetching', error);
-          return throwError(() => new Error(`No se encontró un país con ese nombre: ${query}`));
-        })
-      );
+    return this.http.get<RESTCountry[]>(`${API_URL}/name/${query}`).pipe(
+      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      delay(500),
+      catchError((error) => {
+        console.error('Error fetching', error);
+        return throwError(
+          () => new Error(`No se encontró un país con ese nombre: ${query}`)
+        );
+      })
+    );
+  }
+
+  searchCountryByAlphaCode(code: string) {
+    return this.http.get<RESTCountry[]>(`${API_URL}/alpha/${code}`).pipe(
+      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      map (countries => countries.at(0)),
+      catchError((error) => {
+        console.error('Error fetching', error);
+        return throwError(
+          () => new Error(`No se encontró un país con ese código: ${code}`)
+        );
+      })
+    );
   }
 }
