81621e660ff247a07f3d4b88e616ff1926cdb595
rafafuentesz <rafafuentes110@gmail.com>
2025-08-27 13:38:59 -0300

    Feature: Implemented country search by region and improved loading and error status management.

--- src/app/country/pages/by-region-page/by-region-page.component.ts
+++ src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
-import { SearchInputComponent } from '../../components/search-input/search-input.component';
+import { Component, inject, signal } from '@angular/core';
+import { NgClass, NgFor, TitleCasePipe } from '@angular/common';   // 👈 aquí
 import { CountryListComponent } from '../../components/country-list/country-list.component';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
+import { Region } from '../../interfaces/region.interface';
 
 @Component({
   selector: 'app-by-region-page',
-  imports: [CountryListComponent],
+  standalone: true,
+  imports: [
+    CountryListComponent,
+    NgClass,   // 👈 necesario para usar [ngClass]
+    TitleCasePipe
+  ],
   templateUrl: './by-region-page.component.html',
 })
 export class ByRegionPageComponent {
+  CountryService = inject(CountryService);
 
+  isLoading = signal(false);
+  isError = signal<string | null>(null);
+  countries = signal<Country[]>([]);
+  activeRegion = signal<string | null>(null);
+
+ public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+    'Antarctic',
+  ];
+
+  onSearchByRegion(region: string) {
+    if (this.isLoading()) return;
+
+    this.activeRegion.set(region);
+    this.isLoading.set(true);
+    this.isError.set(null);
+
+    this.CountryService.searchByRegion(region).subscribe({
+      next: (countries) => {
+        this.isLoading.set(false);
+        this.countries.set(countries);
+      },
+      error: (err) => {
+        this.isLoading.set(false);
+        this.countries.set([]);
+        this.isError.set(err);
+      },
+    });
+  }
 }
